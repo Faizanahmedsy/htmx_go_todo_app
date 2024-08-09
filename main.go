5ec057e67ed1f095e4af51459dd302c85b306a88
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-08-09 09:13:57 +0530

    added htmx

--- main.go
+++ main.go
@@ -23,9 +23,19 @@ func main() {
 		temp1 := template.Must((template.ParseFiles("index.html")))
 
 		temp1.Execute(w, data)
+	}
+
+	addTodoHandler := func(w http.ResponseWriter, r *http.Request) {
+		message := r.PostFormValue("message")
+		temp1 := template.Must(template.ParseFiles("index.html"))
+		todo := Todo{Id: len(data["Todos"]) + 1, Message: message}
+		data["Todos"] = append(data["Todos"], todo)
+		temp1.ExecuteTemplate(w, "todo-list-element", todo)
 
 	}
+
 	http.HandleFunc("/", todosHandler)
+	http.HandleFunc("/add-todo", addTodoHandler)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
